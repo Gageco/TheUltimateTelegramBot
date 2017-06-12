21d19529d57a4f694bf85204487f20c1ca1033ef
Gage Coprivnicar <super15gage@gmail.com>
2017-06-12 08:49:58 -0600

    added imports

---
+++ main.go
@@ -0,0 +1,28 @@
+/*
+Bots this includes:
+- Tinbot
+- TheTime
+- Cryptopricebot
+- tgStockBot
+*/
+
+package main
+
+import (
+  "fmt"
+  "flag"
+  "log"
+  "encoding/json"
+  "net/http"
+  "github.com/bot-api/telegram"
+  "github.com/bot-api/telegram/telebot"
+  "golang.org/x/net/context"
+  "strings"
+  "bytes"
+  "io/ioutil"
+  "bufio"
+  "os"
+  "strings"
+  "time"
+  "github.com/mnzt/tinder"
+)
