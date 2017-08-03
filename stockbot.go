70accb97cb6618cc37490ce97d28880dc863952a
Gage Coprivnicar <gagecoprivnicar@MacBook-Pro-5.local>
2017-08-03 11:17:04 -0600

    added some more error handling

--- stockbot.go
+++ stockbot.go
@@ -19,10 +19,15 @@ type singleStock struct {
 
 type quandlDataSet struct {
   Dataset    quandlStock    `json"dataset"`
+  Error      quandlError    `json"quandl_error"`
 }
 
 type quandlStock struct {
   Name    string    `json:"name"`
+}
+
+type quandlError struct {
+  Code    string    `json"code"`
 }
 
 func getStockInfo(stock string, apiKey string) string {
@@ -42,28 +47,38 @@ func getStockInfo(stock string, apiKey string) string {
   data := bytes.TrimSpace(body)
 	data = bytes.TrimPrefix(data, []byte("// "))
 	err = json.Unmarshal(data, &stocks)
-	checkErr(err)
-  // fmt.Println(stocks[0].Exchange)
-  if stocks[0].Exchange == "NASDAQ" || stocks[0].Exchange == "NYSE" || stocks[0].Exchange == "NYSEMKT" {
-    //parse quandl stock api
-    // fmt.Println("in quald junk")
-    quandlUrl := "https://www.quandl.com/api/v3/datasets/WIKI/" + stock + "/metadata.json?api_key=" + apiKey
-    response, err = http.Get(quandlUrl)
-    checkErr(err)
-    defer response.Body.Close()
-    body, err = ioutil.ReadAll(response.Body)
-    checkErr(err)
-    data = bytes.TrimSpace(body)
-    data = bytes.TrimPrefix(data, []byte("// "))
-    err = json.Unmarshal(data, &quandl)
-    checkErr(err)
+  if err != nil {
+    fmt.Println(err)
+    fmt.Println("An error occured on or near like 52, in stockbot.go. The arguement passed was: " + stock)
+    return "An error was found. Please retry or contact @gageco"
+  }
 
+  //parse quandl api
+  quandlUrl := "https://www.quandl.com/api/v3/datasets/WIKI/" + stock + "/metadata.json?api_key=" + apiKey
+  response, err = http.Get(quandlUrl)
+  checkErr(err)
+  defer response.Body.Close()
+  body, err = ioutil.ReadAll(response.Body)
+  checkErr(err)
+  data = bytes.TrimSpace(body)
+  data = bytes.TrimPrefix(data, []byte("// "))
+  err = json.Unmarshal(data, &quandl)
+  if err != nil {
+    fmt.Println(err)
+    fmt.Println("An error occured on or near like 67, in stockbot.go. The arguement passed was: " + stock)
+    return "An error was found. Please retry or contact @gageco"
+  }
+
+  if len(quandl.Dataset.Name) != 0 {
     stockName := quandl.Dataset.Name[:len(quandl.Dataset.Name)-45]
     stringToReturn = stockName + "\nPrice: " + stocks[0].Price + "\n24hr Change: " + stocks[0].Change + "\nExchange: " + stocks[0].Exchange
     fmt.Println("Info For Stock: " + stockName + " Shown.")
-  } else {
+  } else if len(stocks[0].Ticker) != 0 {
+    // fmt.Println("in google stock")
     stringToReturn = stocks[0].Ticker + "\nPrice: " + stocks[0].Price + "\n24hr Change: " + stocks[0].Change + "\nExchange: " + stocks[0].Exchange
     fmt.Println("Info For Stock: " + stocks[0].Ticker + " Shown.")
+  } else {
+    return "Could not find stock, if you believe this was in error please contact @gageco"
   }
 
   return stringToReturn
